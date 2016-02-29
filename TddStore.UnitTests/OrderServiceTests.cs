41b658c2f6456369805c1fd3127808435a9a7af5
Showching <tong.xiaocheng@live.com>
2016-03-01 02:18:25 +0800

    day-12

---
+++ TddStore.UnitTests/OrderServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using TddStore.Core;
+using Telerik.JustMock;
+
+namespace TddStore.UnitTests
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void WhenUserPlacesACorrectOrderThenAnOrderNumberShouldBeReturned()
+        {
+            // Arrange
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.Items.Add(new ShoppingCartItem { ItemId = Guid.NewGuid(), Quantity = 1 });
+            var customerId = Guid.NewGuid();
+            var expectedOrderId = Guid.NewGuid();
+            var orderDataService = Mock.Create<IOrderDataService>();
+            Mock.Arrange(() => orderDataService.Save(Arg.IsAny<Order>())).Returns(expectedOrderId);
+            var orderService = new OrderService(orderDataService);
+
+            // Act
+            var result = orderService.PlaceOrder(customerId, shoppingCart);
+
+            // Assert
+            Assert.AreEqual(expectedOrderId, result);
+        }
+    }
+}
